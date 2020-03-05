659dba301c4d4831ca0f282a218aba5eac6c7163
cebarobot <ceba_robot@outlook.com>
2020-03-05 17:49:58 +0800

    finish: week1

---
+++ week1/15929.神秘的数组初始化.cpp
@@ -0,0 +1,16 @@
+#include <iostream>
+using namespace std;
+
+int main()
+{
+	int * a[] = {
+        NULL, NULL, new int, new int[6]
+    };
+	
+	*a[2] = 123;
+	a[3][5] = 456;
+	if(! a[0] ) {
+		cout << * a[2] << "," << a[3][5];
+	}
+	return 0;
+}
